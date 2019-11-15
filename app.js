3f521dd502fdfb5768eef6c6199287a350b8dc10
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2019-11-15 07:46:25 +0000

    Add todos and home routers

--- app.js
+++ app.js
@@ -24,79 +24,8 @@ db.once('open', () => { // once只觸發一次
   console.log('mongodb connected!')
 })
 
-const Todo = require('./models/todo')
-
-app.get('/', (req, res) => {
-  Todo.find({})
-    .sort({ name: 'asc' })
-    .exec((err, todos) => {
-      if (err) return console.error(err)
-      return res.render('index', { todos: todos })
-    })
-})
-
-app.get('/todos', (req, res) => {
-  return res.redirect('/')
-})
-
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.get('/todos/:id', (req, res) => {
-  Todo.findById(req.params.id, (err, todo) => {
-    if (err) return console.error(err)
-    return res.render('detail', { todo })
-  })
-})
-
-app.post('/todos', (req, res) => {
-//  create = new schema + save
-  const todo = new Todo({
-    name: req.body.name
-  })
-
-  todo.save(err => {
-    if (err) return console.error(err)
-    return res.redirect('/')
-
-    //    Todo.create({name: req.body.name})
-    //    res.redirect('/')
-  })
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  Todo.findById(req.params.id, (err, todo) => {
-    if (err) return console.error(err)
-    return res.render('edit', { todo })
-  })
-})
-
-app.put('/todos/:id', (req, res) => {
-  Todo.findById(req.params.id, (err, todo) => {
-    if (err) return console.error(err)
-    todo.name = req.body.name
-    if (req.body.done === 'on') {
-      todo.done = true
-    } else {
-      todo.done = false
-    }
-    todo.save(err => {
-      if (err) return console.error(err)
-      return res.redirect(`/todos/${req.params.id}`)
-    })
-  })
-})
-
-app.delete('/todos/:id/delete', (req, res) => {
-  Todo.findById(req.params.id, (err, todo) => {
-    if (err) return console.error(err)
-    todo.remove(err => {
-      if (err) return console.error(err)
-      return res.redirect('/')
-    })
-  })
-})
+app.use('/', require('./routes/home'))
+app.use('/todos', require('./routes/todo'))
 
 app.listen(3000, () => {
   console.log('App is listening!')
---
+++ routes/todo.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const router = express.Router()
+const Todo = require('../models/todo')
+
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.get('/:id', (req, res) => {
+  Todo.findById(req.params.id, (err, todo) => {
+    if (err) return console.error(err)
+    return res.render('detail', { todo })
+  })
+})
+
+router.post('', (req, res) => {
+//  create = new schema + save
+  const todo = new Todo({
+    name: req.body.name
+  })
+
+  todo.save(err => {
+    if (err) return console.error(err)
+    return res.redirect('/')
+
+    //    Todo.create({name: req.body.name})
+    //    res.redirect('/')
+  })
+})
+
+router.get('/:id/edit', (req, res) => {
+  Todo.findById(req.params.id, (err, todo) => {
+    if (err) return console.error(err)
+    return res.render('edit', { todo })
+  })
+})
+
+router.put('/:id', (req, res) => {
+  Todo.findById(req.params.id, (err, todo) => {
+    if (err) return console.error(err)
+    todo.name = req.body.name
+    if (req.body.done === 'on') {
+      todo.done = true
+    } else {
+      todo.done = false
+    }
+    todo.save(err => {
+      if (err) return console.error(err)
+      return res.redirect(`/todos/${req.params.id}`)
+    })
+  })
+})
+
+router.delete('/:id/delete', (req, res) => {
+  Todo.findById(req.params.id, (err, todo) => {
+    if (err) return console.error(err)
+    todo.remove(err => {
+      if (err) return console.error(err)
+      return res.redirect('/')
+    })
+  })
+})
+
+module.exports = router
