5952cb0a9045d0f4e3d1874f2f5f8d892599ed7d
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2019-12-04 01:52:30 +0000

    Update new and edit pages for the new project, and tweak relative js and handlebars files

--- routes/record.js
+++ routes/record.js
@@ -9,7 +9,20 @@ router.get('/', authenticated, (req, res) => {
 })
 
 router.get('/new', authenticated, (req, res) => {
-  return res.render('new')
+  // adept Adam Beer's JS code from: https://teamtreehouse.com/community/html-input-date-field-how-to-set-default-value-to-todays-date
+  let date = new Date()
+  let dd = date.getDate()
+  let mm = date.getMonth() + 1 //January is 0!
+  let yyyy = date.getFullYear()
+  if (dd < 10) {
+      dd = '0'+dd
+  } 
+  if (mm < 10) {
+      mm = '0'+mm
+  }
+  date = yyyy + '-' + mm + '-' + dd
+//  console.log(date)
+  return res.render('new', { date })
 })
 
 router.get('/:id', authenticated, (req, res) => {
